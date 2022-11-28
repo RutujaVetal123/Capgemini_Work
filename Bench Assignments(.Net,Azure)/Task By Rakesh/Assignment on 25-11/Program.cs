220b440172c21684dd422d6057ec097bfce51203
Rutuja Vetal <71688207+RutujaVetal123@users.noreply.github.com>
2022-11-28 16:17:26 +0530

    First Commit

---
+++ Bench Assignments(.Net,Azure)/Task By Rakesh/Assignment on 25-11/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticeTask
+{
+    public class Animal
+    {
+        public virtual void eat()
+        {
+            Console.WriteLine("Eating...");
+        }
+    }
+    public class Dog : Animal
+    {
+        public override void eat()
+        {
+            Console.WriteLine("Eating bread...");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dog d = new Dog();
+           // Dog d1 = new Animal();
+            Animal ani = new Dog();
+            Animal ani2 = new Animal();
+            d.eat();
+            ani.eat();
+            ani2.eat();
+
+        }
+    }
+}
